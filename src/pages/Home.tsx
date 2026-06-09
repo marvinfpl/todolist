4637d7410854e4218192d915db95fa65bf69610a
marvinfpl <marvinfoppolo@gmail.com>
2026-06-09 19:52:31 +0200

    animation, better design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,6 +20,15 @@ export default function Home() {
         setInput("");
     }
 
+    function toggleTask(name: string) {
+        setTasks(prev => prev.map(task => {
+            if (task.name === name) {
+                return {...task, done: !task.done}
+            }
+            return task;
+        }))
+    }
+
     function removeTask(name: string) {
         setTasks(prev => prev.filter(task => task.name !== name));
     }
@@ -33,13 +42,13 @@ export default function Home() {
                     e.preventDefault()
                     addTask()
                     }}
-                className="flex flex-col items-center gap-2">
-                    <input value={input} onChange={(e) => setInput(e.target.value)} className="border p-2" placeholder="Enter a task..."/>
-                    <button className="text-white px-3 py-2 rounded">
+                className="flex flex-col items-center gap-3">
+                    <input value={input} onChange={(e) => setInput(e.target.value)} className="border p-2 rounded-xl" placeholder="Enter a task..."/>
+                    <button className="text-white px-3 py-2 rounded transition hover:scale-110">
                         <SquareArrowRight size={32} color="black"/>
                     </button>
                 </form>
-                <Tasks tasks={tasks} removeTask={removeTask}/>
+                <Tasks tasks={tasks} removeTask={removeTask} toggleTask={toggleTask}/>
             </div>
         </div>
     )
