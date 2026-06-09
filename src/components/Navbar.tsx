4aa6490a0eac39b1944747f23b8acfcba2fbbc94
marvinfpl <marvinfoppolo@gmail.com>
2026-06-09 16:02:01 +0200

    icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,15 +4,17 @@ import {Link} from "react-router-dom"
 export default function Navbar() {
     return (
         <nav className="flex justify-around px-6 py-4 bg-slate-900 text-white">
-                <Link to='/about'>
+                <Link to='/about' className="flex flex-col items-center">
                     <Info size={32}/> 
                     <span>About Us</span>  
                 </Link>
-                <Link to='/'>
+                <Link to='/' className="flex flex-col items-center">
                     <House size={32}/>
+                    <span>Home</span>
                 </Link>
-                <Link to='settings'>
-                    <Settings size={32}/>
+                <Link to='settings' className="flex flex-col items-center">
+                    <Settings size={32} />
+                    <span>Settings</span>
                 </Link>
         </nav>
     )
